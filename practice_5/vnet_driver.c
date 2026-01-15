d411e255f073a7688c854681c6cd1e3d99ba47dd
andrehapetr <andrehapetr@yandex.ru>
2026-01-15 20:59:55 +0300

    Step 4

--- practice_5/vnet_driver.c
+++ practice_5/vnet_driver.c
@@ -19,10 +19,12 @@ static const struct net_device_ops vnet_netdev_ops = {
 };
 /* Приватные данные устройства */
 struct vnet_priv {
-  spinlock_t lock;
-  struct sk_buff_head rx_queue;
-  int lost_rx_packets;
-  struct napi_struct napi;
+    spinlock_t lock;
+    struct sk_buff_head rx_queue;
+    int lost_rx_packets;
+    int tx_packets;
+    int rx_packets;
+    struct napi_struct napi;
 };
 
 struct net_device *dev = NULL;
@@ -44,7 +46,7 @@ static int __init vnet_init(void) {
 /* Выход модуля - Очистка всего */
 static void __exit vnet_exit(void) {
   /* Удаление устройства и освобождение ресурсов в обратном порядке*/
-  unregister_netdev(dev);
+  if (dev) unregister_netdev(dev);
   free_netdev(dev);
   pr_info("VNET exit module... Goodbye!\n");
 }
@@ -74,13 +76,35 @@ static void vnet_setup(struct net_device *dev) {
     pr_info("VNET: MAC address: %pM\n", dev->dev_addr);
     // set device flags: Broadcast enabled, Multicast enabled, ARP enabled
     dev->flags |= IFF_BROADCAST | IFF_MULTICAST;
-
     netif_napi_add(dev, &priv->napi, vnet_napi_poll);
+    // napi weight = 64
+    priv->napi.weight = 64;
 }
 
 // napi poll function
 static int vnet_napi_poll(struct napi_struct *napi, int budget) {
-    return 0;
+    struct net_device *netdev = napi->dev;  /* Get from NAPI context */
+    struct vnet_priv *priv = netdev_priv(netdev);
+    struct sk_buff *skb;
+    int work_done = 0;
+    /* Process packets*/
+    spin_lock(&priv->lock);
+    while (work_done < budget && (skb = skb_dequeue(&priv->rx_queue))) {
+        spin_unlock(&priv->lock);
+        pr_info("VNET: Processing received packet of length %u\n", skb->len);
+        /* Simulate packet reception */
+        kfree_skb(skb);
+        done++;
+        spin_lock(&priv->lock);
+    }
+    spin_unlock(&priv->lock);
+
+    if (skb_queue_empty(&priv->rx_queue)) {
+        napi_complete(napi);
+        netif_wake_queue(netdev);
+    }
+
+    return done;
 }
 
 /* Инициализация устройства */
@@ -106,12 +130,34 @@ static int vnet_release(struct net_device *dev)
     return 0;
 }
 
-static int vnet_xmit(struct sk_buff *skb, struct net_device *dev)
+static netdev_tx_t vnet_xmit(struct sk_buff *skb, struct net_device *dev)
 {
-  return 0;
+    struct vnet_priv *priv = netdev_priv(dev);
+
+    //dev->stats.tx_packets++;
+    //dev->stats.tx_bytes += skb->len;
+
+    // Lock rx queue
+    spin_lock(&priv->lock);
+    
+    if (skb_queue_len(&priv->rx_queue) >= 5) {
+        pr_info("VNET: RX queue full, dropping packet\n");
+        spin_unlock(&priv->lock);
+        netif_stop_queue(dev);
+        priv->lost_rx_packets++;
+        kfree_skb(skb);
+        return NETDEV_TX_BUSY;
+    }
+    pr_info("VNET: Packet queued in RX queue\n");
+    // enqueue skb to rx_queue
+    
+    skb_queue_tail(&priv->rx_queue, skb);
+    spin_unlock(&priv->lock);
+    napi_schedule(&priv->napi);
+    
+    return NETDEV_TX_OK;
 }
 
 static int vnet_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
-  return 0;
 }
